6d45a3e57b5fd8acda0b930320771ba1fc190722
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-12-21 21:27:51 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Basic request logging middleware
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+    next();
+});
+
+// Main route
+app.get('/', (req, res) => {
+    res.send('Hello from Kubernetes!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
